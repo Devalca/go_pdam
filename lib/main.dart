8b898f5b02afa5ea414530c0cd70d7d48a328fbf
Amatoxin404 <saepulrahman404@gmail.com>
2022-07-05 20:38:33 +0700

    news

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Pepelakan',
+        title: 'PDAM',
         theme: ThemeData(
             // primaryColor: kPrimaryColor,
             colorScheme:
